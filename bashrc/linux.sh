6389299961631b46247be4d820a0f47be3db5891
yuhui-lin <linyuhuihaha@gmail.com>
2016-11-25 09:55:39 -0700

    add linux trash

--- bashrc/linux.sh
+++ bashrc/linux.sh
@@ -9,3 +9,20 @@ alias la='ls -A --color=auto'
 alias llg='ls -Alh --color=auto | grep -i'
 alias sl='less -im /var/log/messages'
 
+# trash
+if hash trash-put 2>/dev/null; then
+    alias del='gvfs-trash'
+    alias del-dir='trash-list'
+    alias del-empty='trash-empty'
+else
+    alias del='gvfs-trash'
+    # See the content of the trash:
+    # under ~/.local/share/Trash/files/
+    # alias del-dir='gvfs-ls trash://'
+    alias del-dir='ll -Alh --color=auto ~/.local/share/Trash/files/'
+    # Empty the trash:
+    # gvfs-trash --empty
+fi
+if hash gvfs-trash 2>/dev/null; then
+    alias rm='echo Error: Use \"del\" command instead, OR use /bin/rm'
+fi
